11048213d0d1c7467c83d07499749afe040a3856
sarahyakum <sarah1yakum@gmail.com>
2024-10-14 19:02:39 -0500

    static list for classes

--- MauiApp1/Pages/TimeTrack.xaml.cs
+++ MauiApp1/Pages/TimeTrack.xaml.cs
@@ -4,12 +4,27 @@ namespace MauiApp1.Pages;
 
 public partial class TimeTrack : ContentPage
 {
-	
+	 
 
 	public TimeTrack()
 	{
 		InitializeComponent();
+
+		List<string> classes = new List<string>(){
+			"CS4485", "CS4365", "CS4352"
+
+		};
+		listClasses.ItemsSource = classes;
 		
+	}
+
+	private async void OnItemSelected(object sender, SelectedItemChangedEventArgs e){
+		String className = (string)e.SelectedItem;
+		
+		if (className=="CS4485"){ 
+			await Navigation.PushAsync(new Timesheets());
+		}
+		//return className;
 	}
 	
 
---
+++ MauiApp1/Pages/Timesheets.xaml.cs
@@ -0,0 +1,13 @@
+namespace MauiApp1.Pages;
+
+public partial class Timesheets : ContentPage
+{
+	
+
+	public Timesheets()
+	{
+		InitializeComponent();
+	}
+
+}
+
