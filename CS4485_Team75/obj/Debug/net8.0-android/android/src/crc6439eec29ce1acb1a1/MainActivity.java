37216010f1be77009681cdca063950cd525d05bb
sarahyakum <sarah1yakum@gmail.com>
2024-12-03 15:40:59 -0600

    dir

---
+++ CS4485_Team75/obj/Debug/net8.0-android/android/src/crc6439eec29ce1acb1a1/MainActivity.java
@@ -0,0 +1,48 @@
+package crc6439eec29ce1acb1a1;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("CS4485_Team75.MainActivity, CS4485_Team75", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("CS4485_Team75.MainActivity, CS4485_Team75", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("CS4485_Team75.MainActivity, CS4485_Team75", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
